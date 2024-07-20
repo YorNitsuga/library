310e43b613bbd3b3816fcd1c146501b300ebae20
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-07-20 12:09:08 +0800

    Create the form with HTML and add book constructor

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello')
+// Storage for all the books
+const myLibrary = [];
+
+// Constructor for the books
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// Takes user's input and stores it into the storage/array
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
